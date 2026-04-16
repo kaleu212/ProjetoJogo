39f3f1c2c0a803ef5fd067a6ab35bb0dfd97d2c6
Heithor Kaléu <heithorkaleuborges@gmail.com>
2026-04-16 17:40:54 -0300

    teste4

--- index.js
+++ index.js
@@ -25,17 +25,14 @@ window.addEventListener("load", function () {
         let idade = Number(prompt("Digite sua idade:"));
 
 
-        document.addEventListener("DOMContentLoaded", function () {
-            if (idade >= Classificacao) {
-                alert("Acesso liberado ✅");
-            } else {
-                alert("Acesso Negado! Conteúdo Bloqueado 🚫");
+        if (idade >= Classificacao) {
+            alert("Acesso liberado ✅");
+        } else {
+            alert("Acesso Negado! Conteúdo Bloqueado 🚫");
 
-                document.body.style.filter = "blur(10px)";
-                document.body.style.pointerEvents = "none";
-            }
-        });
-
+            document.body.style.filter = "blur(10px)";
+            document.body.style.pointerEvents = "none";
+        }
 
         // marca que já entrou
         localStorage.setItem("primeiraVisita", "true");
