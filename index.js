c2ec4e81d961567a9881eb4bae75e6ad13437e70
Heithor Kaléu <heithorkaleuborges@gmail.com>
2026-04-16 18:04:42 -0300

    revertendo

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const Empresa = "Kaleu Academy";
 const Classificacao = 16;
 
 
-window.onload = function () {
+window.addEventListener("load", function () {
 
-    alert("Bem-vindo ao " + NomeJogo + "!");
+    // verifica se já entrou antes
+    let jaVisitou = localStorage.getItem("primeiraVisita");
 
-    let anoAtual = new Date().getFullYear();
+    if (!jaVisitou) {
 
-    if (anoAtual === AnoDeLancamento) {
-        alert("🎉 Grande lançamento do jogo este ano!");
-    }
+        alert("Bem-vindo ao " + NomeJogo + "!");
+
+        let anoAtual = new Date().getFullYear();
+
+        if (anoAtual === AnoDeLancamento) {
+            alert("🎉 Grande lançamento do jogo este ano!");
+        }
 
         let nome = prompt("Digite seu nome:");
         alert("Olá, " + nome + "! Bem-vindo ao " + NomeJogo);
@@ -29,8 +34,14 @@ window.onload = function () {
             document.body.style.filter = "blur(10px)";
             document.body.style.pointerEvents = "none";
         }
+
+        // marca que já entrou
+        localStorage.setItem("primeiraVisita", "true");
+
+
+
     }
-;
+});
 
 // verifica tema ao carregar a página
 window.onload = function () {
